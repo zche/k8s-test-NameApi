dd02e521f10002779683003489154b919a93d77e
赵锐飞 <peterchezhao@gmail.com>
2020-02-02 19:47:37 +0800

    初始化项目

---
+++ Controllers/NameController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NameApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NameController : ControllerBase
+    {
+        private readonly ILogger<NameController> _logger;
+
+        public NameController(ILogger<NameController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> GetAsync()
+        {
+            var host = Environment.GetEnvironmentVariable("NAME_API_SERVICE_HOST");
+            var port = Environment.GetEnvironmentVariable("NAME_API_SERVICE_PORT");
+            if (string.IsNullOrEmpty(host)) return await Task.FromResult("empty");
+            return await Task.FromResult(host+":"+port);
+        }
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,12 @@
+﻿From mcr.microsoft.com/dotnet/core/sdk:3.1 as build
+WORKDIR /source
+EXPOSE 80
+COPY . ./
+RUN dotnet restore
+RUN dotnet publish -c Release -o out
+
+FROM mcr.microsoft.com/dotnet/core/aspnet:3.1
+RUN ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+WORKDIR /app
+COPY --from=build /source/out /app
+ENTRYPOINT [ "dotnet","NameApi.dll" ]
